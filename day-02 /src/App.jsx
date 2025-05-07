d548b219e80778b9b80132443b0d6dda9227fc17
RachitGandhi-Mern <RachitGandhi.py@gmail.com>
2025-05-07 14:55:43 +0530

    Day-02 first commit

---
+++ day-02 /src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const App = () => {
+  return <div>
+{console.log("hello")}
+  </div>
+}
+
+const App1 = () => {
+  return <div>
+    {console.log("ji")}
+  </div>
+}
+
+export { App, App1 }
